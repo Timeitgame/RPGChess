0d84ddc3f38e2b363a0c3f9d6d127e996ac8f8c4
DR3AM1NGs <106355745+DR3AM1NGs@users.noreply.github.com>
2022-09-15 16:35:03 +0300

    Update BField.cs

--- BField.cs
+++ BField.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BField : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        jgjgjgjg
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+    private void Awake() 
+    { 
         
     }
 }
