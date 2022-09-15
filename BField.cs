8f0eb6c2b2c03bdf7b52deed280ce59d9ea2f02b
Timeitgame <113610844+Timeitgame@users.noreply.github.com>
2022-09-15 16:33:49 +0300

    Update BField.cs

--- BField.cs
+++ BField.cs
@@ -7,7 +7,7 @@ public class BField : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        jgjgjgjg
     }
 
     // Update is called once per frame
